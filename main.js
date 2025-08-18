3ac01e148bfae02081db3e0d877e92ef68ebb9e2
Olena Pletnova <letneva2701@gmail.com>
2025-08-18 17:39:18 +0300

    update files

--- main.js
+++ main.js
@@ -1,45 +1,93 @@
 function DataTable(config, data) {
-  const button = document.createElement("button");
-  button.textContent = "Додати";
-  button.classList.add("add-button");
-  button.onclick = function () { addModalWindow(config) };
-  document.querySelector(config.parent).appendChild(button);
+  document.querySelector(config.parent).appendChild(createButton("Додати", "add-button", "", () => addModalWindow(config)));
 
   const table = document.createElement("table");
 
   table.appendChild(createHead(config));
 
   if (typeof data === "undefined") {
-    getUsers(config.apiUrl).then(users => { table.appendChild(createRow(users, config.columns, config.apiUrl, config)); console.log(users)});
+    getUsers(config.apiUrl).then(users => { table.appendChild(createRow(users, config)); });
   } else {
-    table.appendChild(createRow(data, config.columns));
+    table.appendChild(createRow(data, config));
   }
   document.querySelector(config.parent).appendChild(table);
 }
 
-function addModalWindow(config, element, id) {
+function createButton(content, nameClass, otherNameClass, functionClick) {
+  const button = document.createElement("button");
+  button.textContent = content;
+  button.classList.add(nameClass);
+  if (otherNameClass)   // if button has another class
+    button.classList.add(otherNameClass);
+  button.onclick = functionClick;
+  return button;
+}
+
+function addModalWindow(config, elementOfData, id) {
   const modalWindow = document.createElement("div");
   modalWindow.classList.add("modal-window");
+
   const inputContainer = document.createElement("div");
   inputContainer.classList.add("input-container");
   for (const column of config.columns) {
-    const currentInput = createInput(column, element);
+    const currentInput = createInput(column, elementOfData);
     if (currentInput)
       inputContainer.appendChild(currentInput);
   }
   modalWindow.appendChild(inputContainer);
-  const saveButton = document.createElement("button");
-  saveButton.textContent = "Зберегти";
-  saveButton.classList.add("save-button");
-  saveButton.onclick = function () { saveData(modalWindow, config, element, id) };
-  modalWindow.appendChild(saveButton);
-  const closeButton = document.createElement("button");
-  closeButton.textContent = "Закрити";
-  closeButton.classList.add("save-button");
-  closeButton.classList.add("close-button");
-  closeButton.onclick = function () { closeModalWindow(modalWindow) };
-  modalWindow.appendChild(closeButton);
+
+  const containerButton = document.createElement("div");
+  containerButton.classList.add("button-container");
+  containerButton.appendChild(createButton("Зберегти", "save-button", "", () => saveData(modalWindow, config, elementOfData, id)));
+  containerButton.appendChild(createButton("Закрити", "save-button", "close-button", () => closeModalWindow(modalWindow)));
+  modalWindow.appendChild(containerButton);
   document.body.appendChild(modalWindow);
+}
+
+function createInput(column, elementOfData) {
+  if (!column.input) // if element of columns doesn`t have input field
+    return null;
+
+  const currentInput = document.createElement("div"); // create container for label + input
+  currentInput.classList.add("input-and-label");
+
+  const dataArray = Array.isArray(column.input) ? column.input : [column.input];
+  for (const elementInput of dataArray) {
+
+    const label = document.createElement("label");
+    label.textContent = elementInput.label || column.title;
+
+    let input;
+    if (elementInput.type === "select") {
+      input = document.createElement("select");
+      elementInput.options.forEach(optionValue => {
+        const option = document.createElement("option");
+        option.value = optionValue;
+        option.textContent = optionValue;
+        if (elementOfData)  // if data in tables is edited rather than added, input fields must be automatically filled in
+          input.value = elementOfData.currency;
+        input.appendChild(option);
+      });
+    }
+
+    else {
+      input = document.createElement("input");
+      input.classList.add("current-input");
+      input.type = elementInput.type || "text";
+    }
+
+    input.name = elementInput.name || column.value;
+
+    if (elementOfData) {  // if data in tables is edited rather than added, input fields must be automatically filled in
+      if (input.name === "birthday") {
+        input.value = new Date(elementOfData[input.name]).toISOString().split("T")[0];
+      } else
+        input.value = elementOfData[input.name];
+    }
+    currentInput.appendChild(label);
+    currentInput.appendChild(input);
+  }
+  return currentInput;
 }
 
 async function saveData(modalWindow, config, el, id) {
@@ -48,32 +96,38 @@ async function saveData(modalWindow, config, el, id) {
     if (input.value.trim() === "") {
       input.style.borderColor = "red";
       newValue = null;
+      return null;
     } else {
-    newValue[input.name] = input.value;
-    if (input.name === "price")
-      newValue[input.name] = parseInt(input.value);
-  }
+      newValue[input.name] = input.value;
+      if (input.name === "price")
+        newValue[input.name] = parseInt(input.value);
+    }
   });
-  if(newValue) {
-    let response;
-    if(el) {
-      response = await fetch(config.apiUrl + "/" + id, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newValue)
-      }); } else {
-        response = await fetch(config.apiUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newValue)
-  }); }
-  if (response.ok) {
-    const data = await response.json();
-    alert("Дані збережено:)");
-    document.querySelector(config.parent).innerHTML = "";
-    DataTable(config);
-  } else 
-    alert("Error :(");
+  if (newValue) {
+    let currentMethod;
+    let currentURL;
+
+    if (el) {
+      currentMethod = "PUT";
+      currentURL = `${config.apiUrl}/${id}`;
+    } else {
+      currentMethod = "POST";
+      currentURL = config.apiUrl;
+    }
+
+    const response = await fetch(currentURL, {
+      method: currentMethod,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newValue)
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      alert("Дані збережено:)");
+      document.querySelector(config.parent).innerHTML = "";
+      DataTable(config);
+    } else
+      alert("Error :(");
   }
 }
 
@@ -81,94 +135,51 @@ function closeModalWindow(modalWindow) {
   modalWindow.style.display = "none";
 }
 
-function createInput(column, el) {
-  if (!column.input)
-    return null;
-  const currentInput = document.createElement("div");
-  currentInput.classList.add("current-input");
-  const dataArray = Array.isArray(column.input) ? column.input : [column.input];
-  for (const element of dataArray) {
-    const label = document.createElement("label");
-    label.textContent = element.label || column.title;
-    let input;
-    if (element.type === "select") {
-      input = document.createElement("select");
-      element.options.forEach(optionValue => {
-        const option = document.createElement("option");
-        option.value = optionValue;
-        option.textContent = optionValue;
-        if(el) {
-           input.value = el.currency;
-        }
-        input.appendChild(option);
-      });
-    } else {
-      input = document.createElement("input");
-      input.style.width = "250px";
-      input.style.marginLeft = "10px";
-      input.type = element.type || "text";
-    }
-    input.name = element.name || column.value;
-    if(el) {
-      if (input.name === "birthday") {
-        input.value = new Date(el[input.name]).toISOString().split("T")[0];
-      } else
-        input.value = el[input.name];
-    }
-    currentInput.appendChild(label);
-    currentInput.appendChild(input);
-  }
-  return currentInput;
-}
-
 function createHead(config) {
   const head = document.createElement("thead");
   const row = document.createElement("tr");
   const { columns } = config;
-  for (const key of columns) {
+  for (const column of columns) {
     const cell = document.createElement("th");
-    cell.textContent = key.title;
+    cell.textContent = column.title;
     row.appendChild(cell);
   }
   head.appendChild(row);
   return head;
 }
 
-function createRow(data, columns, apiUrl, config) {
-  let i = 1;
+async function getUsers(apiUrl) {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return Object.entries(data.data);
+}
+
+function createRow(data, config) {
   const body = document.createElement("tbody");
+
   for (const [id, element] of data) {
     const row = document.createElement("tr");
-    for (const key of columns) {
+
+    for (const key of config.columns) {
       const cell = document.createElement("td");
-      if (key.value === "actions") {
-        const button = document.createElement("button");
-        button.textContent = "Видалити";
-        button.style.backgroundColor = "red";
-        button.style.marginRight = "10px";
-        button.onclick = function () { deleteItem(apiUrl + "/" + id, config) };
-        cell.appendChild(button);
-        const buttonEdit = document.createElement("button");
-        buttonEdit.textContent = "Редагувати";
-        buttonEdit.style.backgroundColor = "yellow";
-        buttonEdit.onclick = function () { addModalWindow(config, element, id) };
-        cell.appendChild(buttonEdit);
+      if (key.value === "actions") {   // add buttons "Видалити" and "Редагувати"
+        cell.appendChild(createButton("Видалити", "remove-button", "", () => deleteItem(config.apiUrl + "/" + id, config)));
+        cell.appendChild(createButton("Редагувати", "edit-button", "", () => addModalWindow(config, element, id)));
       }
-      else if (typeof key.value === "function") {
-        if (typeof key.value(element) === "string") {
+      else if (typeof key.value === "function") { // if getAge() or getColor() or img
+        if (typeof key.value(element) === "string") { // if getAge()
           cell.textContent = key.value(element);
-          if (key.value(element).includes("img"))
+          if (key.value(element).includes("img")) // if img
             cell.innerHTML = key.value(element);
         }
-        if (key.value(element) instanceof HTMLElement)
+        if (key.value(element) instanceof HTMLElement) // if getColor()
           cell.appendChild(key.value(element));
       } else {
-        cell.textContent = element[key.value] + " " + id;
+        cell.textContent = element[key.value];
       }
       row.appendChild(cell);
     }
     body.appendChild(row);
-    i++;
   }
   return body;
 }
@@ -183,11 +194,17 @@ async function deleteItem(apiUrl, config) {
   }
 }
 
-async function getUsers(apiUrl) {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return Object.entries(data.data);
-}
+const config1 = {
+  parent: '#usersTable',
+  columns: [
+    { title: 'Ім’я', value: 'name', input: { type: 'text' } },
+    { title: 'Прізвище', value: 'surname', input: { type: 'text' } },
+    { title: 'Вік', value: (user) => getAge(user.birthday), input: { type: "date", name: "birthday", label: "День народження" } },
+    { title: 'Фото', value: (user) => `<img src="${user.avatar}" alt="${user.name} ${user.surname}"/>`, input: { type: "url", name: "avatar" } },
+    { title: 'Дії', value: 'actions' }
+  ],
+  apiUrl: "https://mock-api.shpp.me/opletnova/users"
+};
 
 function getAge(birthday) {
   const currentBirthday = new Date(birthday);
@@ -203,32 +220,6 @@ function getAge(birthday) {
 
   return `years: ${years}, months: ${month}`;
 }
-
-const users = [
-  { id: 30050, name: 'Вася', surname: 'Петров', age: 12 },
-  { id: 30051, name: 'Вася', surname: 'Васечкін', age: 15 },
-];
-
-const config3 = {
-  parent: '#usersTable',
-  columns: [
-    { title: 'Ім’я', value: 'name' },
-    { title: 'Прізвище', value: 'surname' },
-    { title: 'Вік', value: 'age' },
-  ]
-};
-
-const config1 = {
-  parent: '#usersTable',
-  columns: [
-    { title: 'Ім’я', value: 'name', input: { type: 'text' } },
-    { title: 'Прізвище', value: 'surname', input: { type: 'text' } },
-    { title: 'Вік', value: (user) => getAge(user.birthday), input: { type: "date", name: "birthday", label: "День народження" } },
-    { title: 'Фото', value: (user) => `<img src="${user.avatar}" alt="${user.name} ${user.surname}"/>`, input: { type: "url", name: "avatar" } },
-    { title: 'Дії', value: 'actions' }
-  ],
-  apiUrl: "https://mock-api.shpp.me/opletnova/users"
-};
 
 const config2 = {
   parent: '#productsTable',
